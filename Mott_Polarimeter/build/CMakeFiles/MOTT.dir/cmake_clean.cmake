b85c62afffb7849bc2385a522f9df96737f77a45
Martin McHugh <mjmchugh@teller.(none)>
2014-03-31 12:16:29 -0400

    Removed files in build directory

--- Mott_Polarimeter/build/CMakeFiles/MOTT.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/MOTT"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/MOTT.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
