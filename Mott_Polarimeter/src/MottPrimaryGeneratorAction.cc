cbb34a10f0350dce64c2b700b013280a9b1611c9
Martin McHugh <mjmchugh@jlab.org>
2015-02-07 17:03:44 -0500

    Added Methods to deal with Xaviers energy dependent scattering amplitudes (also included). The new stuff is mostly in PrimaryGeneratorAction. It deals with 2D interpolation, better handling of randomization and particle generation and small fixes.

--- Mott_Polarimeter/src/MottPrimaryGeneratorAction.cc
+++ Mott_Polarimeter/src/MottPrimaryGeneratorAction.cc
@@ -55,7 +55,6 @@ MottPrimaryGeneratorAction::MottPrimaryGeneratorAction(
   G4int n_particle = 1;
 
   myMessenger = new MottPrimaryGeneratorMessenger(this);
-
   particleGun = new G4ParticleGun(n_particle);
   
   // Set default 
@@ -67,24 +66,16 @@ MottPrimaryGeneratorAction::MottPrimaryGeneratorAction(
   G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
   G4ParticleDefinition* particle = particleTable->FindParticle("e-");
   particleGun->SetParticleDefinition(particle);
-  
-  //Read in Xavi's data.
-  std::ifstream inFile;
-  inFile.open("/home/mjmchugh/Mott/MottG4/CrossSections/Mott-DWBA-Au-5MeV.out");
-  
-  G4double theta, i, t, u, s;
-  for(G4int nLines=1; nLines<=427; nLines++) {
-    inFile >> theta >> i >> t >> u >> s;
-    ThetaSc.push_back(theta);
-    CrossSection.push_back(i);
-    SpinT.push_back(t);
-    SpinU.push_back(u);
-    Sherman.push_back(s);
-    //G4cout << "\t" << nLines << "\t" << theta << "\t" << i << "\t" << t << "\t" << u << "\t" << s << G4endl;
-  }
-  
-  inFile.close();
-  
+
+  // Resize data vectors
+  ThetaSc.resize(5);
+  CrossSection.resize(5);
+  SpinT.resize(5);
+  SpinU.resize(5);
+  Sherman.resize(5);
+
+  ReadDataFiles();
+
   G4cout << "\tLeaving MottPrimaryGeneratorAction::MottPrimaryGeneratorAction()" <<G4endl;
 }
 
@@ -102,25 +93,36 @@ void MottPrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
   // std::cout << "\tEntering MottPrimaryGeneratorAction::GeneratePrimaries()" << std::endl;
 
   // All Gun settings changed here will change every event. 
-
+  // Random aspects must be input before calling GeneratePrimaryVertex();
+  
   // Source Position
   G4int ThrowFromUpstream = 0;
-  
-  G4ThreeVector gunPosition = G4ThreeVector(0,0,0);
+
+  // Gausian beam profile.  
+  G4double TargetLength = myDetector->GetTargetFullLength();
+  G4double sigma = beamDiameter/(2.354820045*mm);
+
+  G4double X = G4RandGauss::shoot(0.0,sigma)*mm;
+  G4double Y = G4RandGauss::shoot(0.0,sigma)*mm;
+  G4double Z = (G4UniformRand() - 0.5)*TargetLength + myDetector->GetTargetZPosition();
+
+  G4ThreeVector gunPosition = G4ThreeVector(X,Y,Z);
+  G4ThreeVector gunDirection = G4ThreeVector(0.0,0.0,1.0);
   
   // Incident electron's polarization set here
   G4double Sx = 0.0;
-  G4double Sy = 0.0;
-  G4double Sz = 1.0;
+  G4double Sy = 1.0;
+  G4double Sz = 0.0;
   G4ThreeVector preScatteredPolarization = G4ThreeVector(Sx,Sy,Sz);
+
+  // Beam Energy 
+  G4double energy = G4RandGauss::shoot(beamEnergy/(1.0*MeV), energySpread/(1.0*MeV))*MeV;
 
   G4double Phi = 0; 
   G4double Theta = 0;
+  G4double MaxThrow = 8.0e-26;
   if(ThrowFromUpstream) {
-    G4double sigma = beamDiameter/(2.354820045*mm);
-    G4double X = G4RandGauss::shoot(0.0,sigma)*mm;
-    G4double Y = G4RandGauss::shoot(0.0,sigma)*mm;
-    G4double Z = -10.0*cm;
+    Z = -10.0*cm;
     gunPosition = G4ThreeVector(X, Y, Z);
   } else {
     G4int goodThrow = 0;
@@ -137,112 +139,292 @@ void MottPrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
       } else if(0.75<=quadrantRoll&&quadrantRoll<=1.0) {
         Phi = 5.0*G4UniformRand()*deg + 267.5*deg;
       }
-      G4double cs = InterpolateCrossSection(Theta/deg)*(1 + InterpolateSherman(Theta/deg)*cos(Phi));
-      G4double rejectionThrow = 8.4602e-26*G4UniformRand();
-      if(rejectionThrow<=cs) goodThrow = 1; 
+      G4double cs = InterpolateCrossSection(Theta/deg,energy/MeV);
+      G4double s = InterpolateSherman(Theta/deg,energy/MeV);
+      //G4double t = InterpolateT(Theta/deg,energy/MeV);
+      //G4double u = InterpolateU(Theta/deg,energy/MeV);
+      cs = cs*(1 + s*cos(Phi));
+      G4double rejectionThrow = MaxThrow*G4UniformRand();
+      if(rejectionThrow<=cs) {
+      	goodThrow = 1;  
+      	//G4cout << cs << "\t" << s << "\t" << t << "\t" << u << "\t" << G4endl;
+      }
     }
       
-    G4ThreeVector direction;
-                  direction.setRThetaPhi(1.0,Theta,Phi);   
-    particleGun->SetParticleMomentumDirection(direction);
-    G4cout << "\t" << Theta/deg << "\t" << Phi/deg << G4endl;
+    gunDirection.setRThetaPhi(1.0,Theta,Phi);   
+    //G4cout << energy/MeV << "\t" << Theta/deg << "\t" << Phi/deg << "\t" << X/mm << "\t" << Y/mm << "\t" << Z/mm << G4endl;
   }
-  
-  // Beam Energy 
-  G4double energy = G4RandGauss::shoot(beamEnergy/(1.0*MeV), energySpread/(1.0*MeV))*MeV;
  
   // Set variable gun properties
   particleGun->SetParticleEnergy(energy);
   particleGun->SetParticlePosition(gunPosition);
+  particleGun->SetParticleMomentumDirection(gunDirection); 
   particleGun->GeneratePrimaryVertex(anEvent);
-  
-  // Thrown Angle
   
   // std::cout << "\tLeaving MottPrimaryGeneratorAction::GeneratePrimaries()" << std::endl;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-G4double MottPrimaryGeneratorAction::InterpolateCrossSection(G4double x) {
-
-  G4double y = -1;
-
-  if(x<3.7||179.5<x) {
-    G4cout << "Angle outside of range " << x << G4endl;
-    return y;
+void MottPrimaryGeneratorAction::ReadDataFiles() {
+  
+  G4cout << "\tEntering MottPrimaryGeneratorAction::ReadDataFiles()" << G4endl;
+  
+  //Clear old data
+  for(G4int i=0; i<5; i++) {
+    ThetaSc[i].clear();
+    CrossSection[i].clear();
+    SpinT[i].clear();
+    SpinU[i].clear();
+    Sherman[i].clear();
   }
   
-  G4int i=0;
-  while(ThetaSc[i]<x) i++;
-  G4double x1 = ThetaSc[i];	G4double y1 = CrossSection[i];
-  G4double x0 = ThetaSc[i-1];	G4double y0 = CrossSection[i-1];
+  //Read in Xavi's data.
+  std::ifstream dcsFile;				// Cross section files
+  std::ifstream spfFile;				// Spin transfer functions
   
-  y = y0 + (y1-y0)*(x-x0)/(x1-x0);
+  for(G4int i=0; i<5; i++) {  
+  
+    G4String skipLine;
+    G4double theta, mu, dcs, dcs_angstrom;
+    G4double s, t, u, unity;
+    
+    G4double fileEnergy = beamEnergy + 0.05*i-0.10;
+    G4int energyInt = round(beamEnergy/MeV);
+    G4int energyDecimal = 50*i-100;
+    if(i<2) { 
+      energyInt = energyInt-1;
+      energyDecimal += 1000;
+    }
+    
+    char dcsFileName[250], spfFileName[250]; 
+    sprintf(dcsFileName,"/home/mjmchugh/Mott/MottG4/NewMottPhysics/Z=79_E=%4.2fMeV/dcs_%1dp%03de06.dat", fileEnergy, energyInt, energyDecimal);
+    sprintf(spfFileName,"/home/mjmchugh/Mott/MottG4/NewMottPhysics/Z=79_E=%4.2fMeV/spf.dat", fileEnergy);
 
-  return y;
+    dcsFile.open(dcsFileName);
+    for(G4int nLines=1; nLines<27; nLines++) {
+       std::getline(dcsFile, skipLine);
+    }
+  
+    for(G4int nLines=27; nLines<=632; nLines++) {
+      dcsFile >> theta >> mu >> dcs >> dcs_angstrom;
+      ThetaSc[i].push_back(theta);
+      CrossSection[i].push_back(dcs);
+    }
+    dcsFile.close();
+  				
+    spfFile.open(spfFileName);
+    for(G4int nLines=1; nLines<7; nLines++) {
+      std::getline(spfFile, skipLine);
+    }
+
+    for(G4int nLines=7; nLines<=612; nLines++) {
+      spfFile >> theta >> mu >> u >> s >> t >> unity;
+      SpinU[i].push_back(u);
+      Sherman[i].push_back(s);
+      SpinT[i].push_back(t);
+    }
+    spfFile.close();
+  
+  }  
+  
+  G4cout << "\tLeaving MottPrimaryGeneratorAction::ReadDataFiles()" << G4endl;  
+
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-G4double MottPrimaryGeneratorAction::InterpolateSherman(G4double x) {
 
-  G4double y = -1;
+G4double MottPrimaryGeneratorAction::InterpolateCrossSection(G4double theta, G4double energy) {
 
-  if(x<3.7||179.5<x) {
-    G4cout << "Angle outside of range " << x << G4endl;
-    return y;
+  G4double F_xy = -1;
+
+  if(theta<0.0||180.0<theta) {
+    G4cout << "Angle outside of range " << theta << G4endl;
+    return F_xy;
+  }
+  
+  G4double E_lo=-1, E_hi=-1;
+  G4double theta_lo=-1, theta_hi=-1;
+  G4int j_lo=-1, j_hi=-1;
+  G4int i_lo=-1, i_hi=-1;
+
+  G4int j;
+  G4double testEnergy;
+  if (energy < beamEnergy - 0.1) {
+    j_lo = 0;			j_hi = 1;
+    E_lo = beamEnergy - 0.10;	E_hi = beamEnergy -0.05;
+  } else if (energy > beamEnergy + 0.1) {
+    j_lo = 3;			j_hi = 4;
+    E_lo = beamEnergy + 0.05;	E_hi = beamEnergy + 0.10;
+  }  else {
+    for(j=1; j<5; j++) {
+      testEnergy = beamEnergy + (0.05*j - 0.10);
+      if(energy < testEnergy) {
+        E_lo = testEnergy - 0.05;	E_hi = testEnergy;
+        j_lo = j-1;		j_hi = j;
+        break;
+      }
+    }
   }
   
   G4int i=0;
-  while(ThetaSc[i]<x) i++;
-
-  G4double x1 = ThetaSc[i];	G4double y1 = Sherman[i];
-  G4double x0 = ThetaSc[i-1];	G4double y0 = Sherman[i-1];
+  while(ThetaSc[j_lo][i]<theta) i++;
+  i_lo = i-1;			i_hi = i;
+  theta_lo = ThetaSc[j_lo][i-1];	theta_hi = ThetaSc[j_lo][i];
+   
+  G4double F_11 = CrossSection[j_lo][i_lo]*std::abs(E_hi-energy)*std::abs(theta_hi-theta);
+  G4double F_12 = CrossSection[j_lo][i_hi]*std::abs(E_hi-energy)*std::abs(theta_lo-theta);
+  G4double F_21 = CrossSection[j_hi][i_lo]*std::abs(E_lo-energy)*std::abs(theta_hi-theta);
+  G4double F_22 = CrossSection[j_hi][i_hi]*std::abs(E_lo-energy)*std::abs(theta_lo-theta);
+  F_xy = (F_11 + F_12 + F_21 + F_22)/( (E_hi - E_lo)*(theta_hi-theta_lo) );
   
-  y = y0 + (y1-y0)*(x-x0)/(x1-x0);
-
-  return y;
+  return F_xy;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-G4double MottPrimaryGeneratorAction::InterpolateT(G4double x) {
+G4double MottPrimaryGeneratorAction::InterpolateSherman(G4double theta, G4double energy) {
 
-  G4double y = -1;
+  G4double F_xy = -1;
 
-  if(x<3.7||179.5<x) {
-    G4cout << "Angle outside of range " << x << G4endl;
-    return y;
+  if(theta<0.0||180.0<theta) {
+    G4cout << "Angle outside of range " << theta << G4endl;
+    return F_xy;
+  }
+  
+  G4double E_lo=-1, E_hi=-1;
+  G4double theta_lo=-1, theta_hi=-1;
+  G4int j_lo=-1, j_hi=-1;
+  G4int i_lo=-1, i_hi=-1;
+
+  G4int j;
+  G4double testEnergy;
+  if (energy < beamEnergy - 0.1) {
+    j_lo = 0;			j_hi = 1;
+    E_lo = beamEnergy - 0.10;	E_hi = beamEnergy -0.05;
+  } else if (energy > beamEnergy + 0.1) {
+    j_lo = 3;			j_hi = 4;
+    E_lo = beamEnergy + 0.05;	E_hi = beamEnergy + 0.10;
+  }  else {
+    for(j=1; j<5; j++) {
+      testEnergy = beamEnergy + (0.05*j - 0.10);
+      if(energy < testEnergy) {
+        E_lo = testEnergy - 0.05;	E_hi = testEnergy;
+        j_lo = j-1;		j_hi = j;
+        break;
+      }
+    }
   }
   
   G4int i=0;
-  while(ThetaSc[i]<x) i++;
+  while(ThetaSc[j_lo][i]<theta) i++;
+  i_lo = i-1;			i_hi = i;
+  theta_lo = ThetaSc[j_lo][i-1];	theta_hi = ThetaSc[j_lo][i];
+   
+  G4double F_11 = Sherman[j_lo][i_lo]*std::abs(E_hi-energy)*std::abs(theta_hi-theta);
+  G4double F_12 = Sherman[j_lo][i_hi]*std::abs(E_hi-energy)*std::abs(theta_lo-theta);
+  G4double F_21 = Sherman[j_hi][i_lo]*std::abs(E_lo-energy)*std::abs(theta_hi-theta);
+  G4double F_22 = Sherman[j_hi][i_hi]*std::abs(E_lo-energy)*std::abs(theta_lo-theta);
+  F_xy = (F_11 + F_12 + F_21 + F_22)/( (E_hi - E_lo)*(theta_hi-theta_lo) );
 
-  G4double x1 = ThetaSc[i];	G4double y1 = SpinT[i];
-  G4double x0 = ThetaSc[i-1];	G4double y0 = SpinT[i-1];
+  //G4cout << "\t\t" << Sherman[j_lo][i_lo] << "\t" <<  Sherman[j_lo][i_hi] << "\t" <<  Sherman[j_hi][i_lo] << "\t" <<  Sherman[j_hi][i_hi] << G4endl;
+  //G4cout << "i and j: " << i_lo << " " << i_hi << " " << j_lo << " " << j_hi << G4endl;
   
-  y = y0 + (y1-y0)*(x-x0)/(x1-x0);
-
-  return y;
+  return F_xy;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-G4double MottPrimaryGeneratorAction::InterpolateU(G4double x) {
+G4double MottPrimaryGeneratorAction::InterpolateT(G4double theta, G4double energy) {
 
-  G4double y = -1;
+  G4double F_xy = -1;
 
-  if(x<3.7||179.5<x) {
-    G4cout << "Angle outside of range " << x << G4endl;
-    return y;
+  if(theta<0.0||180.0<theta) {
+    G4cout << "Angle outside of range " << theta << G4endl;
+    return F_xy;
+  }
+  
+  G4double E_lo=-1, E_hi=-1;
+  G4double theta_lo=-1, theta_hi=-1;
+  G4int j_lo=-1, j_hi=-1;
+  G4int i_lo=-1, i_hi=-1;
+
+  G4int j;
+  G4double testEnergy;
+  if (energy < beamEnergy - 0.1) {
+    j_lo = 0;			j_hi = 1;
+    E_lo = beamEnergy - 0.10;	E_hi = beamEnergy -0.05;
+  } else if (energy > beamEnergy + 0.1) {
+    j_lo = 3;			j_hi = 4;
+    E_lo = beamEnergy + 0.05;	E_hi = beamEnergy + 0.10;
+  }  else {
+    for(j=1; j<5; j++) {
+      testEnergy = beamEnergy + (0.05*j - 0.10);
+      if(energy < testEnergy) {
+        E_lo = testEnergy - 0.05;	E_hi = testEnergy;
+        j_lo = j-1;			j_hi = j;
+        break;
+      }
+    }
   }
   
   G4int i=0;
-  while(ThetaSc[i]<x) i++;
-
-  G4double x1 = ThetaSc[i];	G4double y1 = SpinU[i];
-  G4double x0 = ThetaSc[i-1];	G4double y0 = SpinU[i-1];
+  while(ThetaSc[j_lo][i]<theta) i++;
+  i_lo = i-1;				i_hi = i;
+  theta_lo = ThetaSc[j_lo][i-1];	theta_hi = ThetaSc[j_lo][i];
+   
+  G4double F_11 = SpinT[j_lo][i_lo]*std::abs(E_hi-energy)*std::abs(theta_hi-theta);
+  G4double F_12 = SpinT[j_lo][i_hi]*std::abs(E_hi-energy)*std::abs(theta_lo-theta);
+  G4double F_21 = SpinT[j_hi][i_lo]*std::abs(E_lo-energy)*std::abs(theta_hi-theta);
+  G4double F_22 = SpinT[j_hi][i_hi]*std::abs(E_lo-energy)*std::abs(theta_lo-theta);
+  F_xy = (F_11 + F_12 + F_21 + F_22)/( (E_hi - E_lo)*(theta_hi-theta_lo) );
   
-  y = y0 + (y1-y0)*(x-x0)/(x1-x0);
+  return F_xy;
+}
 
-  return y;
-}  
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+G4double MottPrimaryGeneratorAction::InterpolateU(G4double theta, G4double energy) {
 
+  G4double F_xy = -1;
+
+  if(theta<0.0||180.0<theta) {
+    G4cout << "Angle outside of range " << theta << G4endl;
+    return F_xy;
+  }
+  
+  G4double E_lo=-1, E_hi=-1;
+  G4double theta_lo=-1, theta_hi=-1;
+  G4int j_lo=-1, j_hi=-1;
+  G4int i_lo=-1, i_hi=-1;
+
+  G4int j;
+  G4double testEnergy;
+  if (energy < beamEnergy - 0.1) {
+    j_lo = 0;			j_hi = 1;
+    E_lo = beamEnergy - 0.10;	E_hi = beamEnergy -0.05;
+  } else if (energy > beamEnergy + 0.1) {
+    j_lo = 3;			j_hi = 4;
+    E_lo = beamEnergy + 0.05;	E_hi = beamEnergy + 0.10;
+  }  else {
+    for(j=1; j<5; j++) {
+      testEnergy = beamEnergy + (0.05*j - 0.10);
+      if(energy < testEnergy) {
+        E_lo = testEnergy - 0.05;	E_hi = testEnergy;
+        j_lo = j-1;			j_hi = j;
+        break;
+      }
+    }
+  }
+  
+  G4int i=0;
+  while(ThetaSc[j_lo][i]<theta) i++;
+  i_lo = i-1;				i_hi = i;
+  theta_lo = ThetaSc[j_lo][i-1];	theta_hi = ThetaSc[j_lo][i];
+    
+  G4double F_11 = SpinU[j_lo][i_lo]*std::abs(E_hi-energy)*std::abs(theta_hi-theta);
+  G4double F_12 = SpinU[j_lo][i_hi]*std::abs(E_hi-energy)*std::abs(theta_lo-theta);
+  G4double F_21 = SpinU[j_hi][i_lo]*std::abs(E_lo-energy)*std::abs(theta_hi-theta);
+  G4double F_22 = SpinU[j_hi][i_hi]*std::abs(E_lo-energy)*std::abs(theta_lo-theta);
+  F_xy = (F_11 + F_12 + F_21 + F_22)/( (E_hi - E_lo)*(theta_hi-theta_lo) );
+  
+  return F_xy;
+}    
 
